dd381a44e55c416f767cc4f78e13645aba214bdd
Scott Essner <scott.essner@gmail.com>
2019-10-07 23:29:19 -0500

    Added movies watch

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from inotify.adapters import InotifyTree
 import inotify.constants as ic
 import json
 import re
+from threading import Thread
 
 
 class Watcher:
@@ -64,5 +65,13 @@ class Watcher:
 
 
 if __name__ == "__main__":
-    w = Watcher('/data/tvshows', r'\.mkv|\.mp4|\.ts\Z', r'\.grab')
-    w.watch_folder()
+    w1 = Watcher('/data/tvshows', r'\.mkv|\.mp4|\.ts\Z', r'\.grab')
+    t1 = Thread(target=w1.watch_folder)
+    t1.start()
+
+    w2 = Watcher('/data/movies', r'\.mkv|\.mp4|\.ts\Z', r'\.grab')
+    t2 = Thread(target=w2.watch_folder)
+    t2.start()
+
+    t1.join()
+    t2.join()
